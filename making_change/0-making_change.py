df553d8dad74731add4baa20839b7aef2a979b06
nuocmam75009 <sayhi.kiseiju@gmail.com>
2025-09-04 15:01:29 +0200

    making change python exercice

---
+++ making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/user/bin/python3
+"""
+Contains the function makeChange(coins, total)
+"""
+
+def makeChange(coins, total):
+    """
+    Make change given a certain total while using the minimum amount of coins
+    Args:
+        coins: list of integers representing coin denominations
+        total: integer representing total amount of change to make
+    Returns:
+        Integer representing the smallest amount of coin to make the given total to change.
+        If total cannot be made with given coins, it returns -1.
+    """
+    if total <= 0:
+        return 0
+    dp = [float('inf') * (total + 1)]
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
+
